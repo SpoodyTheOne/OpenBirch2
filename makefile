a2743e299c2991360fd7b1822a3b2630118e7fdb
SpoodyTheOne <sxsgamer@gmail.com>
2023-08-15 11:10:51 +0200

    Stuff

--- makefile
+++ makefile
@@ -1,3 +1,8 @@
 all:
 	cmake --build build -j12
 	build/src/app/OpenBirch2
+
+clean:
+	rm -rf build
+	mkdir build
+	cmake -S . -B build
--- src/app/main.cpp
+++ src/app/main.cpp
@@ -8,5 +8,9 @@ int main(int argc, char **argv) {
 
   QApplication a(argc, argv);
 
+  MainWindow m;
+
+  m.show();
+
   return a.exec();
 }
