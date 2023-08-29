c0e80b5cf386d0a225af55e7bd9fde308ab8472d
SpoodyTheOne <sxsgamer@gmail.com>
2023-08-29 14:15:40 +0200

    Added document segments

--- src/gui/documentline.cpp
+++ src/gui/documentline.cpp
@@ -1,10 +1,33 @@
 #include "documentline.h"
+#include "documentsegment.h"
 #include "ui_documentline.h"
 
-DocumentLine::DocumentLine(QWidget *parent) : QWidget(parent), ui(new Ui::DocumentLine) {
+DocumentLine::DocumentLine(QWidget *parent, SegmentType type)
+    : QWidget(parent), ui(new Ui::DocumentLine) {
   ui->setupUi(this);
 
-  printf("Added line :3\n");
+  DocumentSegment *segment = DocumentSegment::newFromType(type);
+
+  if (segment == nullptr) {
+    printf("Segment returned nullptr. Aborting\n");
+    abort();
+  }
+
+  ui->horizontalLayout->insertWidget(0, segment->getWidget());
+
+  printf("Added line :3\nType: %d", type);
 }
 
 DocumentLine::~DocumentLine() { printf("Deleted line\n"); }
+
+/** Whether or not this line can be evaluated as an expression by the parser
+ * @brief Whether or not this line is math
+ * @returns bool: true if this line contains exactly 1 Math segment, otherwise
+ * returns false
+ */
+bool DocumentLine::canEvaluate() {
+  if (m_Segments.size() == 1)
+    return m_Segments.begin()->getType() == SegmentType::MATH;
+
+  return false;
+}
--- src/gui/documentline.h
+++ src/gui/documentline.h
@@ -1,6 +1,7 @@
 #ifndef DOCUMENTLINE_H
 #define DOCUMENTLINE_H
 
+#include "documentsegment.h"
 #include <QWidget>
 #include <QObject>
 
@@ -12,11 +13,15 @@ class DocumentLine : public QWidget {
   Q_OBJECT;
 
 public:
-  explicit DocumentLine(QWidget *parent = nullptr);
+  explicit DocumentLine(QWidget *parent = nullptr, SegmentType type = SegmentType::MATH);
   ~DocumentLine();
 
+  bool canEvaluate();
+  
 private:
   Ui::DocumentLine *ui{};
+
+  std::vector<DocumentSegment> m_Segments;
 };
 
 #endif
